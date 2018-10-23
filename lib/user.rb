77e175ea739b8fec080c8c2cbf7136ec1ad2eced
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-23 17:45:36 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,7 @@
 class User
   attr_accessor :first_name, :last_name
-  def initialize(first, last)
-    @first_name = first
-    @last_name = last
+  def initialize#(first, last)
+    # @first_name = first
+    # @last_name = last
   end
 end
